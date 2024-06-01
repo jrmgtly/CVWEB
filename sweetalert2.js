cb7f81fd2f360682f274678b42e697adc997dd60
jrmgtly <156686282+jrmgtly@users.noreply.github.com>
2024-06-01 12:04:05 -0600

    Add files via upload

---
+++ sweetalert2.js
@@ -0,0 +1,51 @@
+function showAlert (){
+    Swal.fire("Welcome recruiters!");
+}
+
+function showAlert1 (){
+    Swal.fire({
+        title: "Software used",
+        text: "Google Workspace - Hubspot - Slack - Trello",
+        icon: "info"
+      });
+}
+
+function showAlert2 (){
+    Swal.fire({
+        title: "Software used",
+        text: "DM9 - Microsoft Excel",
+        icon: "info"
+      });
+}   
+
+function showAlert3 (){
+    Swal.fire({
+        title: "Software used",
+        text: "Zendesk - Jira Atlassian - Looker - Workbench - Admin",
+        icon: "info"    
+      });
+} 
+
+function showAlert4 (){
+    Swal.fire({
+        title: "Software used",
+        text: "Legasuite - Sabre GDS - Salesforce Cloud",
+        icon: "info"    
+      });   
+}
+
+function showAlert5 (){
+    Swal.fire({
+        title: "Software used",
+        text: "Oracle database - Google Workspace - Microsoft Office",
+        icon: "info"    
+      });   
+}  
+
+function showAlert6 (){
+    Swal.fire({
+        title: "Software used",
+        text: "WebCSR - Telegence - Crossroads",
+        icon: "info"    
+      });   
+} 
